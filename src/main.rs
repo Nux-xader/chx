011a483c54fe476474c8db8660f96345efd937ae
Satria Rahmat <sumgmng@gmail.com>
2025-07-14 11:48:49 +0700

    many update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use lzma::LzmaReader;
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use clap::{Command, Arg};
 use std::collections::HashMap;
-use std::io::Read;
 use std::sync::Arc;
-use tokio::sync::{RwLock};
+use tokio::sync::RwLock;
 use std::time::Instant;
+
 
 
 #[tokio::main]
@@ -53,117 +52,63 @@ async fn main() {
 
     loop {
         // Menunggu koneksi dari client
-        let (socket, _) = listener.accept().await.unwrap();
-        println!("New client connected!");
+        let (mut socket, _) = listener.accept().await.unwrap();
+        let store_clone = Arc::clone(&store);
 
         // Menangani client secara asinkron
-        tokio::spawn(handle_client(socket));
-    }
-}
+        tokio::spawn(async move {
+            loop {
+                let mut buffer: [u8; 524288] = [0; 524288];
+                let n = socket.read(&mut buffer).await.unwrap();
 
+                if n == 0 {break;}
 
-async fn handler(mut socket: TcpStream, store_clone: Arc<RwLock<HashMap<Vec<u8>, (Vec<u8>, Instant)>>>, expire_time: u64) {
-    loop {
-        let mut buffer = Vec::with_capacity(10000000);
-        println!("incoming data");
-        let n = socket.read(&mut buffer).await.unwrap();
-        println!("Received {} bytes", n);
-
-        if n == 0 {break;}
-
-        let data = &mut buffer[..n].splitn(2, |&byte| byte == 32);
-        let mut expired_key: Option<Vec<u8>> = None;          
-        match data.next() {
-            Some(action) => {
-                match action {
-                    // GET
-                    [71] => {
-                        let store = store_clone.read().await;
-                        let key = data.next().unwrap_or_default().to_vec();
-                        if let Some((value, timestamp)) = store.get(&key) {
-                            if expire_time > 0 && timestamp.elapsed().as_secs() > expire_time {
-                                expired_key = Some(key);
-                                socket.write_all(&[]).await.unwrap();
-                            } else {
-                                let mut decompressed_data = Vec::new();
-                                {
-                                    let mut reader = LzmaReader::new_decompressor(value.as_slice()).unwrap();
-                                    reader.read_to_end(&mut decompressed_data).unwrap();
+                let data = &mut buffer[..n].splitn(3, |&byte| byte == 32);
+                let mut expired_key: Option<Vec<u8>> = None;
+                match data.next() {
+                    Some(action) => {
+                        match action {
+                            // GET
+                            &[71] => {
+                                let key: Vec<u8> = data.next().unwrap_or_default().trim_ascii().to_vec();
+                                if key.is_empty() {break;}
+                                if let Some((val, timestamp)) = store_clone.read().await.get(&key) {
+                                    if expire_time > 0 && timestamp.elapsed().as_secs() > expire_time {
+                                        expired_key = Some(key);
+                                        socket.write_all(&[33, 101]).await.unwrap();
+                                    } else {
+                                        let mut val_ = val.to_owned();
+                                        val_.insert(0, 62);
+                                        socket.write_all(&val_.as_slice()).await.unwrap();
+                                    }
+                                } else {
+                                    socket.write_all(&[33]).await.unwrap();
                                 }
-                                socket.write_all(decompressed_data.as_slice()).await.unwrap();
                             }
-                        } else {
-                            socket.write_all(&[]).await.unwrap();
-                        }                                            
-                    }
-                    // SET
-                    [83] => {
-                        let mut store = store_clone.write().await;
-                        let key = data.next().unwrap_or_default().to_vec();
-                        let val = data.next().unwrap_or_default();
-                        if key.is_empty() || val.is_empty() {
-                            return;
-                        }
+                            // SET
+                            &[83] => {
+                                let key = data.next().unwrap_or_default().to_vec();
+                                let val = data.next().unwrap_or_default().trim_ascii().to_vec();
+                                if key.is_empty() || val.is_empty() {break;}
 
-                        let mut compressed_data = Vec::new();
-                        {
-                            let mut reader = LzmaReader::new_compressor(val, 5).unwrap();
-                            reader.read_to_end(&mut compressed_data).unwrap();
-                        }
-                        let timestamp = Instant::now();
-                        store.insert(key, (compressed_data, timestamp));
-                        socket.write_all(&[]).await.unwrap();
-                    }
-                    // DEL
-                    [68] => {
+                                let timestamp = Instant::now();
+                                store_clone.write().await.insert(key, (val, timestamp));
+                            }
+                            // DEL
+                            &[68] => {
 
+                            }
+                            _ => {}
+                        }
                     }
-                    _ => {}
+                    None => {}
                 }
+
+                if let Some(key) = expired_key {
+                    store_clone.write().await.remove(&key);
+                }                
             }
-            None => {}
-        }
 
-        if let Some(key) = expired_key {
-            let mut store = store_clone.write().await;
-            store.remove(&key);
-        }                
-    }
-
-}
-
-async fn handle_client(mut stream: TcpStream) {
-
-    loop {
-        let mut buffer = Vec::with_capacity(1024);
-        // Membaca data dari stream (ncat yang mengirimkan data)
-        let bytes_read = stream.read(&mut buffer).await.unwrap();
-
-        if bytes_read == 0 {
-            // Tidak ada data lagi, keluar dari loop
-            break;
-        }
-
-        // Menampilkan data yang diterima dari ncat
-        let received_text = String::from_utf8_lossy(&buffer[..bytes_read]);
-        println!("Received from client: {}", received_text);
-    }
-
-}
-
-#[tokio::main]
-async fn mainx() {
-    let address = "127.0.0.1:8080"; // Alamat dan port untuk server
-    let listener = TcpListener::bind(address).await.unwrap();
-
-    println!("Server listening on {}", address);
-
-    loop {
-        // Menunggu koneksi dari client
-        let (socket, _) = listener.accept().await.unwrap();
-        println!("New client connected!");
-
-        // Menangani client secara asinkron
-        tokio::spawn(handle_client(socket));
+        });
     }
 }
